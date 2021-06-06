b2d2bc8bcf640ef3f950fcef59121f3adf904658
Cedric Matheus <cedric.ms.alves@gmail.com>
2021-06-06 15:15:34 -0300

    Algum refactor que nao sei

---
+++ src/core/components/Character.js
@@ -0,0 +1,9 @@
+import Position from './Position';
+
+/**
+ * Componente que representa um personagem
+ * @property {Position} position - Posição do personagem
+ */
+const Character = { position: { ...Position } };
+
+export default Character;
---
+++ src/core/components/Position.js
@@ -0,0 +1,8 @@
+/**
+ * Componente que representa uma posição 2D
+ * @property {number} x=0 - Posição no eixo X
+ * @property {number} y=0 - Posição no eixo Y
+ */
+const Position = { x: 0, y: 0 };
+
+export default Position;
